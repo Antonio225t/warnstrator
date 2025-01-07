a28e07dc62be6905a9878a28fa5081f0eb8bf7d5
GitHub Action <action@github.com>
2025-01-07 17:05:13 +0000

    Initialize template repository proper
    Set package name to com.github.antonio225t.warnstrator
    Set project name to warnstrator
    Set modid to warnstrator

--- src/main/java/com/github/antonio225t/warnstrator/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/antonio225t/warnstrator/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.antonio225t.warnstrator.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
--- src/main/java/com/github/antonio225t/warnstrator/mixin/MixinGuiMainMenu.java
+++ src/main/java/com/github/antonio225t/warnstrator/mixin/MixinGuiMainMenu.java
@@ -1,4 +1,4 @@
-package com.example.mixin;
+package com.github.antonio225t.warnstrator.mixin;
 
 import net.minecraft.client.gui.GuiMainMenu;
 import org.spongepowered.asm.mixin.Mixin;
